b4481cf4e378b4fea507585aa642c08b812c7033
lnxen <m18338151940@163.com>
2025-05-20 20:40:18 +0800

    Add files via upload

---
+++ Contest.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define debug(x) cout << #x << "=" << x << endl;
+#define _for(i, a, b) for (int i = a; i < b; i++)
+#define _ifor(i, a, b) for (int i = a; i >= b; i--)
+#define _forr(i, a, b) for (int i = a; i <= b; i++)
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define INF 0x3f3f3f3f
+#define pi 3.14
+#define ff first
+#define ss second
+using namespace std;
+const int N = 1e6 + 10;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+void solve(){
+	
+    return;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+
+	return 0;
+} 
